645843b7416e4f33d93a326758c34fa5a4936ff4
Oluwafisayomi Ogunyemi <oluwafisayomiogunyemi@gmail.com>
2020-12-04 09:49:45 +0100

    Create New.java

---
+++ New.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int fellow = 1;
+        System.out.println(fellow);
+        String Fisayo = "Fisayo";
+        System.out.println(Fisayo);
+
+    }
+}
